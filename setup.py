c1955d29feccb484544ade98098bccca7ff28ca1
Maxim <drotxx@yandex.ru>
2022-12-03 17:49:29 +0400

    setup.py update

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='touch_input',
+setup(name='touch-input',
       version='1.0.2',
       description='Python Touch Input',
       author='',
       author_email='',
       python_requires=">=3.10, <4",
-      url='https://github.com/sleppyKid/touch_input.git',
+      url='https://github.com/sleppyKid/touch-input.git',
       packages=['touch_input'],
       install_requires=[],
       )
